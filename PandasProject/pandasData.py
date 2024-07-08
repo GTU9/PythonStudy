15093d26bb628aa8be65aa9d4dc7e31b28b51d72
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-07-08 16:44:43 +0900

    .

--- PandasProject/pandasData.py
+++ PandasProject/pandasData.py
@@ -1,3 +1,29 @@
 import pandas as pd
-df = pd.read_csv("naver.csv")
-df.head(15)
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://www.billboard.com/charts/hot-100/')
+yc_web_page = response.text
+soup = BeautifulSoup(yc_web_page, "html.parser")
+
+article_tag = soup.find_all(name="h3", id="title-of-a-story")
+
+song_name=[]
+rank=[]
+rank_default=0
+
+for article in article_tag:
+    song=article.getText()
+    song_name.append(song.strip())
+    rank_default+=1
+    rank.append(rank_default)
+
+song_data = {
+    'rank':rank,
+    'song_name':song_name,
+}
+
+
+df = pd.DataFrame(song_data)
+
+print(df.head(10))
